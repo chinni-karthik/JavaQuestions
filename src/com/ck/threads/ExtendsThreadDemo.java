7756b2fc19bd9ef1082bd46d78324b7841761d18
karthik chinni <kaarthik.chinni@gmail.com>
2018-01-22 23:57:51 +0530

    Threads Basic programs
    Basic two thread programs

---
+++ src/com/ck/threads/ExtendsThreadDemo.java
@@ -0,0 +1,19 @@
+package com.ck.threads;
+
+public class ExtendsThreadDemo {
+	public static void main(String[] args) {
+		MyThread t = new MyThread();
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main Thread running...!!!");
+		}
+	}
+}
+
+class MyThread extends Thread {
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child Thread Running...!!!");
+		}
+	}
+}
