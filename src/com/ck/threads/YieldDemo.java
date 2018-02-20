3f26450a5c6939332aa2f202bd983b13ca09a991
karthik chinni <kaarthik.chinni@gmail.com>
2018-02-21 01:35:30 +0530

    Changes to yield() for better explanation.

--- src/com/ck/threads/YieldDemo.java
+++ src/com/ck/threads/YieldDemo.java
@@ -1,11 +1,14 @@
 package com.ck.threads;
 
+//Note : The thread that wants to wait has to call join(), on the thread object that it is ready to wait for. 
+//Very simple. Did not know why you got confused...!!!
 public class YieldDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Thread t = new MyThreadYealdDemo();
 		t.start();
+		t.join();//Rama has to wait until sita completes her execution. Hence, rama called join() on sita Thread.
 		for (int i = 0; i < 10; i++) {
-			System.out.println("Main Thread Running...!!!");
+			System.out.println("Rama Thread");
 		}
 	}
 }
@@ -13,8 +16,10 @@ public class YieldDemo {
 class MyThreadYealdDemo extends Thread {
 	public void run() {
 		for (int i = 0; i < 10; i++) {
-			Thread.yield();
-			System.out.println("Child Thread Running...!!!");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {}
+			System.out.println("Sita Thread");
 		}
 	}
 }
