565416a22ff6cb7c9ee5c64065449e95d4888380
karthik chinni <kaarthik.chinni@gmail.com>
2018-02-25 00:06:56 +0530

    Solved HCL Questions

--- src/com/ck/unable/to/solve/ExistingMapToRangeMapCount.java
+++ src/com/ck/unable/to/solve/ExistingMapToRangeMapCount.java
@@ -1,5 +1,43 @@
 package com.ck.unable.to.solve;
 
-public class ExistingMapToRangeMapCount {
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 
+public class ExistingMapToRangeMapCount {
+	public static void main(String[] args) {
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("1000", 1);
+		map.put("1050", 3);
+		map.put("1460", 6);
+		map.put("2000", 4);
+		map.put("2040", 1);
+		map.put("2400", 3);
+
+		HashMap<String, Integer> outputMap = new HashMap<>();
+		
+		Set<Entry<String, Integer>> entrySet = map.entrySet();
+		for (Entry<String, Integer> entry : entrySet) {
+			String effectiveKey = getEffectiveKey(entry.getKey());
+			if(outputMap.get(effectiveKey)==null){
+				outputMap.put(effectiveKey, map.get(entry.getKey()));
+			}else{
+				//Splitted for better readability
+				int valOne=map.get(entry.getKey());
+				int valTwo=outputMap.get(effectiveKey);
+				outputMap.put(effectiveKey, valOne+valTwo);
+			}
+		}
+		Set<Entry<String, Integer>> outputEntrySet = outputMap.entrySet();
+		for (Entry<String, Integer> entry : outputEntrySet) {
+			System.out.println(entry.getKey()+"---"+entry.getValue());
+		}
+	}
+	public static String getEffectiveKey(String actualKey){
+		char[] actualKeyCharArray = actualKey.toCharArray();
+		for (int i = 1; i < actualKeyCharArray.length; i++) {
+			actualKeyCharArray[i]='0';
+		}
+		return String.valueOf(actualKeyCharArray);
+	}
 }
--- src/com/ck/unable/to/solve/TwoThreadsCalculatingOneExpression.java
+++ src/com/ck/unable/to/solve/TwoThreadsCalculatingOneExpression.java
@@ -1,5 +1,37 @@
 package com.ck.unable.to.solve;
 
 public class TwoThreadsCalculatingOneExpression {
-
+	/*
+	Expression to calculate:
+	(1+2)/(1*2)
+	t1-->(1+2)
+	t2-->(1*2)
+	t3-->(t1.value/t2.value)
+	*/
+	public static void main(String[] args) throws InterruptedException {
+		Add t1=new Add();
+		Mul t2=new Mul();
+		t1.start();
+		t2.start();
+		//By this time, add and mul would have been computed by two threads.
+		//Making main thread wait until t1 and t2 are completed.
+		t1.join();
+		t2.join();
+		//Printed by main thread...!!!
+		System.out.println(t1.value/t2.value);
+	}
+}
+class Add extends Thread{
+	int value;
+	@Override
+	public void run(){
+		value= 1+2;
+	}
+}
+class Mul extends Thread{
+	int value;
+	@Override
+	public void run(){
+		value= 1*2;
+	}
 }
